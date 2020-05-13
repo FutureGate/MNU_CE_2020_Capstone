c78207ad529730efe3dd398648003bf12598bbba
Futuregate <futuregate0815@gmail.com>
2020-05-14 01:59:14 +0900

    db 수정 및 데이터 저장 코드(임시) 추가

--- Web/database.sql
+++ Web/database.sql
@@ -3,6 +3,21 @@ create database if not exists `demand_prediction`;
 use `demand_prediction`;
 
 -- ----------------------------------------------
+-- 판매점 테이블
+-- ----------------------------------------------
+
+create table SHOP_TABLE (
+	`shop_id`					integer auto_increment not null,
+    `contact`					varchar(40),
+    `email`						varchar(40),
+	`about`						varchar(200),
+    primary key(shop_id)
+) default character set utf8mb4 collate utf8mb4_bin;
+
+
+
+
+-- ----------------------------------------------
 -- 유저 테이블
 -- ----------------------------------------------
 
@@ -10,13 +25,11 @@ create table USER_TABLE (
     `user_id`					varchar(10) unique not null,
 	`user_name`					varchar(20) not null,
     `password`					varchar(20) not null,
-    `company`					varchar(30) not null,
-    `email`						varchar(30),
-    `contact`					varchar(20) not null,
-    `about`						varchar(200),
+    `shop_id`					integer not null,
     `registered_date`			date not null,
     `user_level`				integer not null,
-    primary key(user_id)
+    primary key(user_id),
+    foreign key (shop_id) references SHOP_TABLE (shop_id)
 ) default character set utf8mb4 collate utf8mb4_bin;
 
 
@@ -27,14 +40,33 @@ create table USER_TABLE (
 
 create table SALE_TABLE (
 	`sale_id`					integer auto_increment not null,
-	`user_id`					varchar(10) not null,
 	`sale_date`					date not null,
-    `prod_code`					varchar(10) not null,
-    `prod_name`					varchar(20) not null,
+	`shop_id`					integer not null,
+    `item_id`					integer not null,
     `sale_count`				integer not null,
-    primary key(sale_id),
-    foreign key (user_id) references USER_TABLE (user_id)
+    primary key(sale_id)
 ) default character set utf8mb4 collate utf8mb4_bin;
+
+
+
+
+
+
+
+
+
+-- ----------------------------------------------
+-- 상품 테이블
+-- ----------------------------------------------
+
+create table ITEM_TABLE (
+	`item_id`					integer auto_increment not null,
+	`item_name`					varchar(20) not null,
+	`shop_id`					integer not null,
+    primary key(item_id),
+    foreign key (shop_id) references SHOP_TABLE (shop_id)
+) default character set utf8mb4 collate utf8mb4_bin;
+
 
 
 
